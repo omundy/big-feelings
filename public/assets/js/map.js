f215f43366c51dce28555349b41e0c2a8705a137
Owen Mundy <omundy@gmail.com>
2025-08-13 21:09:18 -0400

    Format

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -7,84 +7,84 @@ let markerLayer = [];
 
 // INITIALIZE MAP
 var map = L.map("map", {
-  center: [20, -25],
-  zoom: 3,
+    center: [20, -25],
+    zoom: 3,
 });
 
 // url for tileset
 let tiles =
-  "https://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}";
+    "https://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}";
 tiles = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
 
 // add tile layer
 L.tileLayer(tiles, {
-  maxZoom: 14,
-  minZoom: 2,
-  // noWrap: true,
-  // bounds: [
-  //   [-90, -180],
-  //   [90, 180]
-  // ],
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 14,
+    minZoom: 2,
+    // noWrap: true,
+    // bounds: [
+    //   [-90, -180],
+    //   [90, 180]
+    // ],
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 /**
  *  Called (by main.js) on load and after form submission
  */
-function updateMap(data) {  
-  
-  // 👉 add code inside this function (Chapter 10) ...
-  
-  // remove all the markers
-  removeMarkers();
+function updateMap(data) {
 
-  // loop through JSON
-  for (let i = 0; i < data.length; i++) {
-    // console.log(data[i]);
-    
-    // create marker and add to array
-    markerLayer[i] = createMarker(data[i]);
-  }
-  
-  // 👈
+    // 👉 add code inside this function (Chapter 10) ...
+
+    // remove all the markers
+    removeMarkers();
+
+    // loop through JSON
+    for (let i = 0; i < data.length; i++) {
+        // console.log(data[i]);
+
+        // create marker and add to array
+        markerLayer[i] = createMarker(data[i]);
+    }
+
+    // 👈
 }
 
 /**
- *  Create and add feeling   marker to map
+ *  Create and add feeling marker to map
  */
 function createMarker(row) {
-  let popupContent = `<span class="popup" style="color:${row.color};">${row.feeling}</span>`;
-  let color = row.color;
-  let marker = L.circleMarker([row.lat, row.lng], {
-    radius: 50,
-    stroke: false,
-    color: row.color,
-    fillOpacity: getFillOpacity(),
-    className: "blur",
-  });
- let popup = L.popup({className: "hello"}).setContent(popupContent);
-  marker
-    .addTo(map).bindPopup(popup)
-    .on('click', function(){
-      console.log("createMarker()", color)
-      // updatePopupColor(color)
+    let popupContent = `<span class="popup" style="color:${row.color};">${row.feeling}</span>`;
+    let color = row.color;
+    let marker = L.circleMarker([row.lat, row.lng], {
+        radius: 50,
+        stroke: false,
+        color: row.color,
+        fillOpacity: getFillOpacity(),
+        className: "blur",
     });
-  return marker;
+    let popup = L.popup({ className: "hello" }).setContent(popupContent);
+    marker
+        .addTo(map).bindPopup(popup)
+        .on('click', function () {
+            console.log("createMarker()", color)
+            // updatePopupColor(color)
+        });
+    return marker;
 }
 
 
-function updatePopupColor(color){
-      console.log("updatePopupColor()", color)
-  let wrapper = document.querySelector(".leaflet-popup-content-wrapper");
-  let tip = document.querySelector(".leaflet-popup-tip");  
-  wrapper.style.backgroundColor = color;
-  tip.style.backgroundColor = color;
-  
-  
-  // let parent = document.querySelector(".leaflet-popup");
-  // parent.style.backgroundColor = color;
-  
+function updatePopupColor(color) {
+    console.log("updatePopupColor()", color)
+    let wrapper = document.querySelector(".leaflet-popup-content-wrapper");
+    let tip = document.querySelector(".leaflet-popup-tip");
+    wrapper.style.backgroundColor = color;
+    tip.style.backgroundColor = color;
+
+
+    // let parent = document.querySelector(".leaflet-popup");
+    // parent.style.backgroundColor = color;
+
 }
 
 
@@ -96,16 +96,16 @@ function updatePopupColor(color){
 // web form inside popup
 let inputMarker;
 let inputMarkerPopup = L.popup()
-      .setLatLng(L.latLng([0,0]))
-      .setContent(getInputMarkerPopup(L.latLng([0,0])))
-      // .openOn(map);
+    .setLatLng(L.latLng([0, 0]))
+    .setContent(getInputMarkerPopup(L.latLng([0, 0])))
+// .openOn(map);
 
 
 
 
-function getInputMarkerPopup(latlng, show=false){
-  
-  return `<form class="popupInput">
+function getInputMarkerPopup(latlng, show = false) {
+
+    return `<form class="popupInput">
   
           <div class="row">
             <div class="col">
@@ -164,44 +164,44 @@ function getInputMarkerPopup(latlng, show=false){
 
 
 map.on("click", (e) => {
-  // wrap latlng (reset coordinates on dateline -180 and +180 degrees)
-  let latlng = L.latLng([e.latlng.lat, e.latlng.lng]).wrap();
-  // prevent specific locations
-  latlng.lat = Number(latlng.lat.toPrecision(5));
-  latlng.lng = Number(latlng.lng.toPrecision(5));
+    // wrap latlng (reset coordinates on dateline -180 and +180 degrees)
+    let latlng = L.latLng([e.latlng.lat, e.latlng.lng]).wrap();
+    // prevent specific locations
+    latlng.lat = Number(latlng.lat.toPrecision(5));
+    latlng.lng = Number(latlng.lng.toPrecision(5));
 
-  // if (!inputMarker) {
-  //   // inputMarker doesn't yet exist
-  //   inputMarker = L.marker(latlng).addTo(map);
-  // } else {
-  //   // thereafter, update its position
-  //   inputMarker.setLatLng(latlng).update();
-  // }
+    // if (!inputMarker) {
+    //   // inputMarker doesn't yet exist
+    //   inputMarker = L.marker(latlng).addTo(map);
+    // } else {
+    //   // thereafter, update its position
+    //   inputMarker.setLatLng(latlng).update();
+    // }
 
-  
 
-  console.log(latlng);
-  // inputMarkerPopup.setLatLng(latlng).setContent(latlng.toString()).openOn(map);
-  inputMarkerPopup.setLatLng(latlng);
-  map.openPopup(inputMarkerPopup);
-  // updatePopupColor("white");
-  let location = document.querySelector("#location");
-  location.value = `${latlng.lat},${latlng.lng}`;
-  
-  document.querySelector(".submitBtn").addEventListener("click", submitForm);
-  updateOptions(colors);
+
+    console.log(latlng);
+    // inputMarkerPopup.setLatLng(latlng).setContent(latlng.toString()).openOn(map);
+    inputMarkerPopup.setLatLng(latlng);
+    map.openPopup(inputMarkerPopup);
+    // updatePopupColor("white");
+    let location = document.querySelector("#location");
+    location.value = `${latlng.lat},${latlng.lng}`;
+
+    document.querySelector(".submitBtn").addEventListener("click", submitForm);
+    updateOptions(colors);
 });
 
 // close popups on zoom
 map.on("zoomstart", (e) => {
-  // map.closePopup();
+    // map.closePopup();
 });
 
 // update marker opacity
 map.on("zoomend", (e) => {
-  markerLayer.forEach(function (marker, i) {
-    marker.setStyle({ fillOpacity: getFillOpacity() });
-  });
+    markerLayer.forEach(function (marker, i) {
+        marker.setStyle({ fillOpacity: getFillOpacity() });
+    });
 });
 
 // // constrain viewport to map tiles (SW, NE) on drag
@@ -217,14 +217,14 @@ map.on("zoomend", (e) => {
 
 // removes all markers from the map
 function removeMarkers() {
-  // remove visible marker
-  for (let i = 0; i < markerLayer.length; i++) {
-    map.removeLayer(markerLayer[i]);
-  }  
-  // empty the array
-  markerLayer = [];
+    // remove visible marker
+    for (let i = 0; i < markerLayer.length; i++) {
+        map.removeLayer(markerLayer[i]);
+    }
+    // empty the array
+    markerLayer = [];
 }
 
 function getFillOpacity() {
-  return map.getZoom() / 25;
+    return map.getZoom() / 25;
 }
